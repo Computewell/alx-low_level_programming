e821a22950cbf6154b54c8b718548c5ae6dc7a75
Ololade Akinloye <98171019+Loladecodes2@users.noreply.github.com>
2022-07-01 05:52:08 -0800

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,41 +1,21 @@
 #include <stdio.h>
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+int z;
 
-	int z;
-
-
-
-	for (z = 48; z <= 57; z++)
-
-	{
-
-		putchar(z);
-
-	}
-
-	for (z = 97; z <= 102;z++)
-
-	{
-
-		putchar(z)
-
-	}
-
-	putchar('\n');
-
-	return (0);
-
+for (z = 48; z <= 57; z++)
+{
+putchar(z);
+}
+for (z = 97; z <= 102;z++)
+{
+putchar(z)
+}
+putchar('\n');
+return (0);
 }
