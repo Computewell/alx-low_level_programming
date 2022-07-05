79644880c9923598f6e4e19ca97f65ce7e8a995e
Ololade Akinloye <98171019+Loladecodes2@users.noreply.github.com>
2022-07-05 09:48:47 -0800

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,39 +1,20 @@
 /*
+ * File: 1-alphabet.c
  *
- *  * File: 1-alphabet.c
- *
- *   *
- *
- *    * Auth: Dr Marcus imagwe
- *
- *     */
-
-
+ * Auth: Dr Marcus imagwe
+ */
 
 #include "main.h"
 
-
-
 /**
- *
- *  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
- *
- *   */
-
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
 void print_alphabet(void)
-
 {
+	char alpha;
 
-		char alpha;
+	for (alpha = 97; alpha <= 122; alpha++)
+		_putchar(alpha);
 
-
-
-			for (alpha = 97; alpha <= 122; alpha++)
-
-						_putchar(alpha);
-
-
-
-				_putchar('\n');
-
+	_putchar('\n');
 }
