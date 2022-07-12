ab383b7b35b8ee811451f8aecf9dd4161c735bf0
Ololade Akinloye <98171019+Loladecodes2@users.noreply.github.com>
2022-07-12 04:19:32 -0800

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _puts - print a string 
- * @str: the string 
+ * _puts - prints a string
+ * @str: the string
  *
  * Return: the length of the string
  */
 void _puts(char *str)
-{ 
+{
 	int i = 0;
 
 	while (str[i] != '\0')
