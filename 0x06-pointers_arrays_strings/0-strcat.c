4a1672cb79c884360f8859ff2e69b731d254e505
Ololade Akinloye <98171019+Loladecodes2@users.noreply.github.com>
2022-07-14 04:40:18 -0800

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,16 +7,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int dlen = 0, i;
-while (dest[dlen])
-{
-dlen++;
-}
-for (i = 0; src[i] != 0; i++)
-{
-dest[dlen] = src[i];
-dlen++;
-}
-dest[dlen] = '\0';
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++)
+;
+
+do {
+	a++;
+	dest[i] = src[a];
+	i++;
+} while (src[a] != '\0');
+
 return (dest);
 }
