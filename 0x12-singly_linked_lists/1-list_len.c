eac20c7c03a30629ac091a033390491e933ee6c2
Ololade Akinloye <ololade220@gmail.com>
2022-08-11 04:55:18 -0800

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,22 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * list_len - Finds the number of elements in
+ *            a linked list_t list.
+ * @h: The linked list_t list.
+ * @elements: a counter var for size_t (nodes)
  *
- * Return: number of nodes
+ * Return: The number of elements in h.
  */
-size_t listint_len(const listint_t *h)
+size_t list_len(const list_t *h)
 {
-	size_t num = 0;
+	size_t elements = 0;
 
 	while (h)
 	{
-		num++;
+		elements++;
 		h = h->next;
 	}
 
-	return (num);
+	return (elements);
 }
